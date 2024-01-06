2a6fcbbf4ef52459c7e0a8ac4f4b386dce86da5c
NikolaiKostadinov <nikolaikostadinov123@abv.bg>
2024-01-06 11:56:11 +0200

    test

---
+++ index.js
@@ -0,0 +1,79 @@
+
+//for(let step = 0; step < 5; step++) {
+//console.log(`this is step Number ${step}`);
+//}
+
+ // const text = 'Hello World'
+ // for(let step = 0; step < text.length; step++) {
+ //     console.log(`this is step Number ${step} and ${text[step]}`);
+ //     }
+
+ // function iterateObject(indexedObject) {
+ //    let result = 0
+// 
+ //    for (let index = 0; index < indexedObject.length; index++) {
+ //        result += indexedObject[index]
+ //    }
+// 
+//     return result
+//  }
+
+
+//  console.log(iterateObject([1, 2, 3, 4, 6]));
+
+
+
+
+ // function iterateObject(indexedObject) {
+   //   let resultArray = []
+
+   //  for (let index = 0; index < indexedObject.length; index++) {
+   //       if (indexedObject[index] < 3)
+   //       resultArray.push(indexedObject[index])
+   //   }
+
+  //    return resultArray 
+ //  }
+
+
+  // console.log(iterateObject([1, 2, 3, 4, 6]));
+             // 1 task
+// for (let step= 1; step < 6; step ++) {
+//    console.log(step)
+// }
+             
+
+           // 2 task 
+ // let result = 0
+ // for (let index = 0; index <= 10; index++) {
+ //    
+ //      result += index
+ //    
+ // }
+ // console.log(result);
+          
+                      // 3 task
+         // presmqtane na chisla 2,4,6,8,10 !!!! // 
+
+  // for (let number = 2; number <= 10; number = number + 2) {
+  //    
+  //    console.log(number);
+  // }
+
+ // for (let number = 1; number <=100; number = number + 1) {
+  //      console.log(number);
+  // }
+
+ // for (let n = 1; n <= 10; n = n + 3 ) {
+   //    console.log(n);
+
+ // }
+
+// for (let number = 2; number <= 26; number = number + 2 ) {
+  //       console.log(number);
+ // }
+
+
+ // for (let n = 1; n <= 20; n= n + 1) {
+  //     console.log(n);
+ // }
